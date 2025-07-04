c033f324687b47aee9fcb0f07faa6c7487842cd7
Kimseungin0529 <whffkaos007@naver.com>
2025-07-04 21:06:28 +0900

    chore : 프로젝트 생성

---
+++ src/main/java/project/springbatchtoy/SpringBatchToyApplication.java
@@ -0,0 +1,13 @@
+package project.springbatchtoy;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBatchToyApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringBatchToyApplication.class, args);
+    }
+
+}
---
+++ src/test/java/project/springbatchtoy/SpringBatchToyApplicationTests.java
@@ -0,0 +1,13 @@
+package project.springbatchtoy;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBatchToyApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
