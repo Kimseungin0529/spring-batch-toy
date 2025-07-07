e6760c67f6142110b2d646f72f358f9a94cd028a
Kimseungin0529 <whffkaos007@naver.com>
2025-07-07 23:18:47 +0900

    feat : Product entity 생성, application.yml 설정

--- src/main/java/project/springbatchtoy/batch/domain/Product.java
+++ src/main/java/project/springbatchtoy/batch/domain/Product.java
@@ -1,4 +1,15 @@
 package project.springbatchtoy.batch.domain;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import lombok.Getter;
+
+@Entity
+@Getter
 public class Product {
+    @Id
+    private Long id;
+    private String name;
+    private int price;
+    private String type;
 }
