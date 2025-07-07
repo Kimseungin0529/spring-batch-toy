c9f2835e64a02eca43f3b4e15601c6e9d408febe
Kimseungin0529 <whffkaos007@naver.com>
2025-07-07 22:54:33 +0900

    feat : ProductVO 필드 추가

--- src/main/java/project/springbatchtoy/batch/domain/ProductVO.java
+++ src/main/java/project/springbatchtoy/batch/domain/ProductVO.java
@@ -1,4 +1,17 @@
 package project.springbatchtoy.batch.domain;
 
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Getter
+@NoArgsConstructor
+@AllArgsConstructor
 public class ProductVO {
+    private Long id;
+    private String name;
+    private int price;
+    private String type;
+
+
 }
