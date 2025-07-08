249eb4564d889e9db10e5fe13b82d67457e6d136
Kimseungin0529 <whffkaos007@naver.com>
2025-07-08 10:36:51 +0900

    feat : API 호출/처리에 대한 itemReader 구현

--- src/main/java/project/springbatchtoy/batch/job/api/ApiStepConfig.java
+++ src/main/java/project/springbatchtoy/batch/job/api/ApiStepConfig.java
@@ -2,15 +2,24 @@ package project.springbatchtoy.batch.job.api;
 
 import lombok.RequiredArgsConstructor;
 import org.springframework.batch.core.Step;
+import org.springframework.batch.core.configuration.annotation.StepScope;
 import org.springframework.batch.core.repository.JobRepository;
 import org.springframework.batch.core.step.builder.StepBuilder;
+import org.springframework.batch.item.ItemReader;
+import org.springframework.batch.item.database.JdbcPagingItemReader;
+import org.springframework.batch.item.database.Order;
+import org.springframework.batch.item.database.support.MySqlPagingQueryProvider;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.jdbc.core.BeanPropertyRowMapper;
 import org.springframework.transaction.PlatformTransactionManager;
 import project.springbatchtoy.batch.domain.ProductVO;
 import project.springbatchtoy.batch.partition.ProductPartitioner;
 
 import javax.sql.DataSource;
+import java.util.HashMap;
+import java.util.Map;
 
 @Configuration
 @RequiredArgsConstructor
@@ -46,5 +55,32 @@ public class ApiStepConfig {
     @Bean
     public ProductPartitioner partitioner() {
         return new ProductPartitioner(dataSource);
+    }
+
+
+    @Bean
+    @StepScope
+    public ItemReader<ProductVO> itemReader(@Value("#{stepExecutionContext['product']}") ProductVO productVO) throws Exception {
+
+        JdbcPagingItemReader<ProductVO> reader = new JdbcPagingItemReader<>();
+
+        reader.setDataSource(dataSource);
+        reader.setPageSize(CHUNK_SIZE);
+        reader.setRowMapper(new BeanPropertyRowMapper(ProductVO.class));
+
+        MySqlPagingQueryProvider queryProvider = new MySqlPagingQueryProvider();
+        queryProvider.setSelectClause("id, name, price, type");
+        queryProvider.setFromClause("from product");
+        queryProvider.setWhereClause("where type = :type");
+
+        Map<String, Order> sortKeys = new HashMap<>(1);
+        sortKeys.put("id", Order.DESCENDING);
+        queryProvider.setSortKeys(sortKeys);
+
+        reader.setParameterValues(QueryGenerator.getParameterForQuery("type", productVO.getType()));
+        reader.setQueryProvider(queryProvider);
+        reader.afterPropertiesSet();
+
+        return reader;
     }
 }
